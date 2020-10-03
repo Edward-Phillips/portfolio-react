b3ac4c6e667e1dfe88cd074e8128d06f98dcfd29
Edward Phillips <edd0ddi0@gmail.com>
2020-10-03 16:51:40 +0100

    add placeholder homepage component

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to my Portfolio</h1>
+      <section>
+        <p>Currently my portfolio website is a work in progress, however if you'd like to see some of my work please do check out my <a href='https://github.com/Edward-Phillips'>Github</a></p>
+      </section>
+    </div>
+  )
+}
+
+export default Home
